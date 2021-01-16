b7ec5fc0c6b1660a221785bd8c72f6fbaeaa029f
Chris Lee <chris@levonica.io>
2021-01-17 02:01:18 +0900

    typescript version

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,25 +4,34 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
 // Add this import at the top of the file
 import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths } from "next";
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
-}
+};
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
-}
+};
 
-export default function Post({ postData }) {
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
   return (
     <Layout>
       <Head>
